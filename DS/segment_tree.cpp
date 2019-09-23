cd902b045d81d4f504d3686c8fa5be175f9fbfb8
Darkhan Turgyn <darustur@gmail.com>
2019-09-23 17:33:50 +0600

    fix

--- DS/segment_tree.cpp
+++ DS/segment_tree.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 int n;
-int t[];           //! update tree size -> 4*n
+int t[], a[];           //! update tree size -> 4*n
 
-void built(int a[], int v, int tl, int tr){
-    if(tl == tr) t[v] = tl;
+void built(int v, int tl, int tr){
+    if(tl == tr) t[v] = a[tl];
     else{
         int tm = (tl + tr) / 2;
-        built(a, v*2, tl, tm);
-        built(a, v*2+1, tm+1, tr);
+        built(v*2, tl, tm);
+        built(v*2+1, tm+1, tr);
         t[v] = t[v*2] + t[v*2+1]; 
     }
 }
@@ -36,5 +36,6 @@ void update(int v, int tl, int tr, int pos, int new_val){
 
 int main(){
     cin>>n;
-    int tree[4*n];
+    int t[4*n];
+    int a[n];
 }
